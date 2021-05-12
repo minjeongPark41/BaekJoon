5ee9322ca9f194dc3d6a9c76cb0cd3f615b9c80a
minjeongPark41 <pmjabcd@gmail.com>
2021-05-12 23:59:30 +0900

    Level1 - BJ1008
    Level1 - BJ1008

---
+++ BJ/src/Level1/l1008.java
@@ -0,0 +1,19 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class l1008 {
+
+	public static void main(String[] args) {
+
+				Scanner scan = new Scanner (System.in);
+				
+				double A = scan.nextDouble();
+				double B = scan.nextDouble();
+				scan.close();
+				
+				System.out.print(A/B);
+
+	}
+
+}
