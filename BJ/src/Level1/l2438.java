055184f9af7f1189c78ff8ca566dee6f611ee656
minjeongPark41 <pmjabcd@gmail.com>
2021-07-15 23:05:45 +0900

    l2438
    l2438

---
+++ BJ/src/Level1/l2438.java
@@ -0,0 +1,25 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class l2438 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner (System.in);
+		int N = scan.nextInt();
+		
+		for (int i=0; i<N; i++) { 
+			for (int j=0; j<i+1; j++) {
+				System.out.print("*");
+			}
+				System.out.println();
+		}
+
+	}
+
+}
+
+		//*
+		//**
+		//***
