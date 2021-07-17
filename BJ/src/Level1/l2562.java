64fe407ad48da14f29783fb4f643683e3988d4d1
minjeongPark41 <pmjabcd@gmail.com>
2021-07-17 17:34:54 +0900

    2562

---
+++ BJ/src/Level1/l2562.java
@@ -0,0 +1,17 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class l2562 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner (System.in);
+		int N = scan.nextInt();
+		
+		System.out.printf("최댓값:%d\, 몇 번째 수:%s ", max, number);
+		
+
+	}
+
+}
